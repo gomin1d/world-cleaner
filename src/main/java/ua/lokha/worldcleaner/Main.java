458dc5b3538e4819fa5163a0f4dab4d649d3cad4
lokha <aleksey.kovalchuk98@gmail.com>
2020-06-08 16:04:05 +0300

    Git init

---
+++ src/main/java/ua/lokha/worldcleaner/Main.java
@@ -0,0 +1,11 @@
+package ua.lokha.worldcleaner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Test");
+    }
+}
